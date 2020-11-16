0e80f52c3d888dd6f788d30d7985a450768bf364
Your Name <you@example.com>
2020-11-15 23:28:14 -0500

    Test Classes

--- src/ListingTest.java
+++ src/ListingTest.java
@@ -1,4 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.BeforeEach.*;
+import static org.junit.jupiter.api.AfterEach.*;
+import static org.junit.jupiter.api.Test.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeEach.*;
+import org.junit.jupiter.api.AfterEach.*;
+import org.junit.jupiter.api.Test.*;
 
 public class ListingTest {
 
+	
+	
+	@BeforeEach
+	public void setup() {
+		
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void TestAddAmmenity(){
+		ArrayList<amenity> amenities = amenities.add();
+	}
+	
+	@Test
+	public void TestReturnAmenities(){
+		
+	}
+	
+	@Test
+	public void TestFoundAmmenityMatch() {
+		ArrayList<amenity> amenities ;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
